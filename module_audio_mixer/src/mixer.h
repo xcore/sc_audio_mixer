2a553b75b8b2cc4768ce8dfcf8f33e66f51ccd54
Ross Owen <ross@xmos.com>
2012-02-01 15:43:08 +0000

    Updates to mixer.h

--- module_audio_mixer/src/mixer.h
+++ module_audio_mixer/src/mixer.h
@@ -1,11 +1,17 @@
 
-#include "mixerparams.h"
+#ifndef MIXER_NUM_CHAN_IN
+#define MIXER_NUM_CHAN_IN       (3)
+#endif
 
-#define MIXER_MAX_VOL            0x20000000
+#ifndef MIXER_NUM_CHAN_OUT
+#define MIXER_NUM_CHAN_OUT      (2)
+#endif
+
+#define MIXER_MAX_VOL            (0x20000000)
 
 /* Supported mixer commands */
-#define MIXER_CMD_MIX_MULT       10
-#define MIXER_CMD_KILL           11
+#define MIXER_CMD_MIX_MULT       (10)
+#define MIXER_CMD_KILL           (11)
 
 /* Mixer thread */
 int Mixer(chanend c_in, chanend c_out, chanend c_ctrl);
